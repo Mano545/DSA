6c17fa42da21707784fc428174b7194ed09ba03e
Manoranjith T U <tgbmano535@gmail.com>
2026-06-19 10:20:49 +0530

    Added solution - LeetHub

---
+++ 713976--GFG/713976-.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> optimalArray(vector<int> &arr) {
+        vector<int>ans;
+        ans.push_back(0);
+        for(int i=1;i<arr.size();i++)
+        {
+            ans.push_back(ans.back()+(arr[i]-arr[i/2]));
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
