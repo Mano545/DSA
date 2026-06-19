76c38de57dfa5b550314934b2c334108c9fd99cf
Manoranjith T U <tgbmano535@gmail.com>
2026-06-19 09:27:28 +0530

    Time: 420 ms (60.89%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-combine-two-tables/0001-combine-two-tables.sql
@@ -0,0 +1,6 @@
+select firstName,lastName,city,state from person p left join Address a on p.personId = a.personId;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
